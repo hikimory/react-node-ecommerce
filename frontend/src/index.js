872176d91cab8ecaf72cb53a4b16c41b2dd619f3
Nick Bykon <nikita.bikon@gmail.com>
2020-08-17 16:03:47 +0300

    add redux library

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from './store'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+)
+
+render(app,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
