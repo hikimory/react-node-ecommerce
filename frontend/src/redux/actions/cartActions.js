9b7c75a2557f1622402bf529cb0ac6a05c092f4e
Nick Bykon <nikita.bikon@gmail.com>
2020-08-21 15:43:50 +0300

    remove unused variables

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -29,7 +29,6 @@ const removeFromCart = (productId) => (dispatch, getState) => {
     dispatch({ type: CART_REMOVE_ITEM, payload: productId });
 
     const { cart: { cartItems } } = getState();
-    console.log(cartItems)
     Cookie.set("cartItems", JSON.stringify(cartItems));
 }
 
